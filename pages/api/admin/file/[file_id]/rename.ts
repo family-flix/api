36d862f7d9f6c9a3e07731fa2213796a3a3c968a
ltaoo <litaowork@aliyun.com>
2023-09-17 01:10:59 -0500

    release: v1.14.3 (#27)
    Feat
    1. 今日新增影片接口返回影片发布时间
    
    Bugfix
    1. 无法新增文件夹名称不同的分享资源的问题

--- pages/api/admin/file/[file_id]/rename.ts
+++ pages/api/admin/file/[file_id]/rename.ts
@@ -13,19 +13,19 @@ import { Drive } from "@/domains/drive";
 export default async function handler(req: NextApiRequest, res: NextApiResponse<BaseApiResp<unknown>>) {
   const e = response_error_factory(res);
   const { authorization } = req.headers;
-  const { file_id } = req.query as Partial<{ file_id: string }>;
+  const { file_id, drive_id } = req.query as Partial<{ file_id: string; drive_id: string }>;
   const { name } = req.body as Partial<{ name: string }>;
+  const t_res = await User.New(authorization, store);
+  if (t_res.error) {
+    return e(t_res);
+  }
+  const user = t_res.data;
   if (!file_id) {
     return e(Result.Err("缺少文件 file_id"));
   }
   if (!name) {
     return e(Result.Err("缺少新的文件名"));
   }
-  const t_res = await User.New(authorization, store);
-  if (t_res.error) {
-    return e(t_res);
-  }
-  const user = t_res.data;
   const file_res = await store.find_file({
     file_id,
     user_id: user.id,
@@ -35,10 +35,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   }
   const file = file_res.data;
   if (!file) {
+    if (drive_id) {
+      const drive_res = await Drive.Get({ id: drive_id, user, store });
+      if (drive_res.error) {
+        return e(drive_res);
+      }
+      const drive = drive_res.data;
+      const r = await drive.client.rename_file(file_id, name);
+      if (r.error) {
+        return e(r);
+      }
+    }
     return e(Result.Err("没有匹配的记录"));
   }
-  const { drive_id } = file;
-  const drive_res = await Drive.Get({ id: drive_id, user, store });
+  const drive_res = await Drive.Get({ id: file.drive_id, user, store });
   if (drive_res.error) {
     return e(drive_res);
   }
