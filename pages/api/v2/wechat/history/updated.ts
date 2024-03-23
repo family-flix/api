b76a4affb029fdd41b9aee919768b9eb0c155a5e
ltaoo <litaowork@aliyun.com>
2024-03-23 22:52:25 +0800

    feat: optimize the API fetch recently updated histories

--- pages/api/v2/wechat/history/updated.ts
+++ pages/api/v2/wechat/history/updated.ts
@@ -8,128 +8,90 @@ import { store } from "@/store/index";
 import { Member } from "@/domains/user/member";
 import { BaseApiResp } from "@/types/index";
 import { response_error_factory } from "@/utils/server";
+import { MediaTypes } from "@/constants/index";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<BaseApiResp<unknown>>) {
   const e = response_error_factory(res);
   const { authorization } = req.headers;
-  const { next_marker = "", page_size = 20 } = req.body as Partial<{ next_marker: string; page_size: number }>;
+  const { page = 1, page_size = 20 } = req.body as Partial<{ page: number; page_size: number }>;
   const t_res = await Member.New(authorization, store);
   if (t_res.error) {
     return e(t_res);
   }
   const member = t_res.data;
-  //   const r: {
-  //     id: string;
-  //     text: string;
-  //     duration: number;
-  //     current_time: number;
-  //     thumbnail_path: string;
-  //     file_id: string;
-  //     media_id: string;
-  //     media_source_id: string;
-  //     member_id: string;
-  //     profile_name: string;
-  //     profile_poster_path: string;
-  //     latest_episode_order: number;
-  //   }[] = await store.prisma.$queryRaw`
-  // SELECT DISTINCT m1.*, m4.id AS profile_id, m4.name AS profile_name, m4.poster_path AS profile_poster_path, latest_media_source.latest_episode_order
-  // FROM PlayHistoryV2 m1
-  // JOIN (
-  //   SELECT
-  //       m2.id as media_id,
-  //       MAX(m3.created) AS max_media_source_created,
-  //       m2.profile_id,
-  //       m3.episode_order as latest_episode_order
-  //   FROM
-  //       Media m2
-  //   JOIN (
-  //     SELECT mm3.*, mm4.\`order\` AS episode_order
-  //     FROM MediaSource mm3
-  //     JOIN MediaSourceProfile mm4 ON mm3.profile_id = mm4.id
-  //   ) m3 ON m2.id = m3.media_id
-  //   GROUP BY
-  //       m2.id
-  // ) latest_media_source
-  // ON m1.media_id = latest_media_source.media_id
-  // JOIN MediaProfile m4 ON latest_media_source.profile_id = m4.id
-  // WHERE m1.member_id = '${member.id}' AND m1.updated < latest_media_source.max_media_source_created
-  // ORDER BY m1.updated DESC
-  // LIMIT 10;
-  // `;
   const r: {
     id: string;
     name: string;
     poster_path: string;
     updated: string;
+    cur_episode_order: number;
+    cur_episode_name: number;
+    thumbnail_path: string;
     latest_episode_order: number;
     latest_episode_name: number;
+    latest_episode_created: string;
   }[] = await store.prisma.$queryRaw`
 SELECT
     Media.id AS id,
-    COUNT(MediaSource.media_id) AS count,
     MediaProfile.name AS name,
     MediaProfile.poster_path AS poster_path,
+    CurEpisode.cur_order AS cur_episode_order,
+    CurEpisode.cur_name AS cur_episode_name,
     PlayHistoryV2.updated AS updated,
-    MAX(MediaSourceProfile.\'order\') AS latest_episode_order,
+    PlayHistoryV2.thumbnail_path AS thumbnail_path,
     MediaSource.created AS latest_episode_created,
+    MAX(MediaSourceProfile.\'order\') AS latest_episode_order,
     MediaSourceProfile.name AS latest_episode_name
 FROM MediaSource
 JOIN Media ON Media.id = MediaSource.media_id
 JOIN MediaSourceProfile ON MediaSource.profile_id = MediaSourceProfile.id
 JOIN MediaProfile ON Media.profile_id = MediaProfile.id
 JOIN PlayHistoryV2 ON PlayHistoryV2.media_id = Media.id
-WHERE Media.id = PlayHistoryV2.media_id AND MediaSource.created > PlayHistoryV2.updated AND PlayHistoryV2.member_id = ${
-    member.id
-  } AND PlayHistoryV2.updated > ((strftime('%s', 'now') * 1000) - (60 * 60 * 24 * 60 * 1000))
+JOIN (
+  SELECT
+    MediaSourceProfile.\'order\' AS cur_order,
+    MediaSourceProfile.name AS cur_name,
+    PlayHistoryV2.id AS history_id
+  FROM MediaSource
+  JOIN MediaSourceProfile ON MediaSource.profile_id = MediaSourceProfile.id
+  JOIN PlayHistoryV2 ON PlayHistoryV2.media_source_id = MediaSource.id
+) CurEpisode ON PlayHistoryV2.id = CurEpisode.history_id
+WHERE Media.id = PlayHistoryV2.media_id AND Media.type = ${
+    MediaTypes.Season
+  } AND MediaSource.created > PlayHistoryV2.updated AND PlayHistoryV2.member_id = ${member.id}
 GROUP BY MediaSource.media_id
 ORDER BY PlayHistoryV2.updated DESC, MediaSourceProfile.\'order\' DESC
-LIMIT ${page_size + 1}
+LIMIT ${page_size} OFFSET ${(page - 1) * page_size}
 `;
-
-  // const data = await client.$queryRaw`
-  // SELECT
-  //     Media.id AS id,
-  //     MediaProfile.name AS name,
-  //     MediaSourceProfile.name AS episode_name,
-  //     MediaSourceProfile.\'order\',
-  //     MediaSource.created
-  // FROM MediaSource
-  // JOIN Media ON Media.id = MediaSource.media_id
-  // JOIN PlayHistoryV2 ON PlayHistoryV2.media_id = Media.id
-  // JOIN MediaSourceProfile ON MediaSource.profile_id = MediaSourceProfile.id
-  // JOIN MediaProfile ON MediaProfile.id = MediaSourceProfile.media_profile_id
-  // WHERE MediaSource.created > PlayHistoryV2.updated
-  // GROUP BY MediaProfile.id
-  // ORDER BY PlayHistoryV2.updated DESC, MediaSourceProfile.\'order\' DESC
-  // LIMIT 10
-  // `;
-  // console.log(data)
-
-  // const data1 = await client.$queryRaw`
-  // SELECT MediaSource.media_id, MediaSource.id, Media.text AS name, MAX(MediaSourceProfile.\'order\') AS latest_episode_order
-  // FROM MediaSource
-  // JOIN Media ON Media.id = MediaSource.media_id
-  // JOIN MediaSourceProfile ON MediaSource.profile_id = MediaSourceProfile.id
-  // JOIN PlayHistoryV2 ON PlayHistoryV2.media_id = Media.id
-  // WHERE Media.id = PlayHistoryV2.media_id AND MediaSource.created > PlayHistoryV2.updated
-  // GROUP BY MediaSource.media_id
-  // ORDER BY PlayHistoryV2.updated DESC, MediaSourceProfile.\'order\' DESC
-  // LIMIT 20
-  // `
   const data = {
     list: r.map((media) => {
-      const { id, name, poster_path, latest_episode_order, latest_episode_name, updated } = media;
+      const {
+        id,
+        name,
+        poster_path,
+        cur_episode_name,
+        cur_episode_order,
+        thumbnail_path,
+        latest_episode_order,
+        latest_episode_name,
+        latest_episode_created,
+        updated,
+      } = media;
       return {
         id,
         name,
         poster_path,
+        cur_episode_name,
+        cur_episode_order: Number(cur_episode_order),
+        updated,
+        thumbnail_path,
         latest_episode_order: Number(latest_episode_order),
         latest_episode_name,
-        updated,
+        latest_episode_created,
       };
     }),
-    next_marker: "",
-    no_more: true,
+    no_more: r.length < page_size,
+    page,
     page_size,
   };
   res.status(200).json({
