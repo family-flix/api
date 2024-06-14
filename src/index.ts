d9d1770ea867d11b6fef9b4a73b49c1c021620a2
ltaoo <litaowork@aliyun.com>
2024-06-14 15:10:19 +0800

    feat: proxy API add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { IncomingMessage } from "http";
 
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 import { serve } from "@hono/node-server";
 
 import { compat_next } from "./utils/server";
@@ -256,6 +257,17 @@ async function main() {
   server.get("/api/proxy", async (c) => {
     return v0_proxy(...(await compat_next(c)));
   });
+  server.use(
+    "/api/proxy",
+    cors({
+      origin: "https://hlsjs.video-dev.org/",
+      allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
+      allowMethods: ["POST", "GET", "OPTIONS"],
+      exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+      maxAge: 600,
+      credentials: true,
+    })
+  );
   /** 管理后台 */
   server.post("/api/admin/user/login", async (c) => {
     return v0_admin_user_login(...(await compat_next(c)));
