472765e9946f636e0ce763a28717b3dd7705f27b
ltaoo <litaowork@aliyun.com>
2024-07-16 21:54:18 +0800

    fix: fetch media profile in douban after getting the media ranks of douban

--- src/domains/media_rank/index.ts
+++ src/domains/media_rank/index.ts
@@ -138,43 +138,37 @@ export function MediaRankClient(props: { app: Application<any>; user: User; stor
       // });
       for (let i = 0; i < list.length; i += 1) {
         const { douban_id } = list[i];
-        await (async () => {
-          if (!douban_id) {
-            return;
-          }
-          const d1 = await store.prisma.media_profile.findFirst({
-            where: {
-              douban_id,
-            },
-          });
-          if (d1) {
-            return;
-          }
-          const r = await client.fetch_media_profile(douban_id, {});
-          if (r.error) {
-            console.log("client.fetch_media_profile failed", r.error.message);
-            return;
-          }
-          if (!r.data.name) {
-            return;
-          }
-          const d2 = await store.prisma.media_series_profile.findFirst({
-            where: {
-              name: r.data.name,
-              original_name: r.data.original_name,
-              air_date: r.data.air_date,
-            },
-          });
-          if (d2) {
-            return;
-          }
-          // if (r.data.type === "movie") {
-          //   await $profile.search_movie({ keyword: r.data.name, year: r.data.air_date });
-          // }
-          // if (r.data.type === "tv") {
-          //   await $profile.search_season({ keyword: r.data.name, season_text: null, year: r.data.air_date });
-          // }
-        })();
+        // await (async () => {
+        //   if (!douban_id) {
+        //     return;
+        //   }
+        //   const d1 = await store.prisma.media_profile.findFirst({
+        //     where: {
+        //       douban_id,
+        //     },
+        //   });
+        //   if (d1) {
+        //     return;
+        //   }
+        //   const r = await client.fetch_media_profile(douban_id, {});
+        //   if (r.error) {
+        //     console.log("client.fetch_media_profile failed", r.error.message);
+        //     return;
+        //   }
+        //   if (!r.data.name) {
+        //     return;
+        //   }
+        //   const d2 = await store.prisma.media_series_profile.findFirst({
+        //     where: {
+        //       name: r.data.name,
+        //       original_name: r.data.original_name,
+        //       air_date: r.data.air_date,
+        //     },
+        //   });
+        //   if (d2) {
+        //     return;
+        //   }
+        // })();
         const r = await link_media_with_rank_media(list[i], {
           type: values.type === "movie" ? MediaTypes.Movie : MediaTypes.Season,
         });
